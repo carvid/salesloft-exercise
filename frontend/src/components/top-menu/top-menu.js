e3d0dd52a2923ef4a4ca6f273cd3f11d4db315e6
Carlos David Gonzalez Abraham <carlosdavid@gonzalez-abraham.com.mx>
2020-03-08 13:46:02 -0600

    Add top-menu component.
    This presentational component renders a menu with the following options:
    People, Email Characters, and Duplicates.

---
+++ frontend/src/components/top-menu/top-menu.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Icon, Menu, Table } from 'semantic-ui-react';
+
+class TopMenu extends Component {
+  state = {
+    active: 'people',
+  }
+
+  render() {
+    const { active } = this.state;
+
+    return(
+      <Menu>
+        <Menu.Item
+          key='people'
+          name='People'
+          as={Link}
+          to="people"
+          active={active === 'people'}
+          onClick={() => { this.setState({active: 'people' }) }}
+        />
+        <Menu.Item
+          key='email-chars'
+          name='Email Chars'
+          as={Link}
+          to="email-chars"
+          active={active === 'email-chars'}
+          onClick={() => { this.setState({active: 'email-chars' }) }}
+        />
+        <Menu.Item
+          key='duplicates'
+          name='Duplicates'
+          as={Link}
+          to="duplicates"
+          active={active === 'duplicates'}
+          onClick={() => { this.setState({active: 'duplicates' }) }}
+        />
+      </Menu>
+    );
+  }
+}
+
+export default TopMenu;
